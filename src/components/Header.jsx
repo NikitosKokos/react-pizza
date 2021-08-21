1f2027c09960db64279ece0fd1da3a708ac3900b
Nikita Tsykunov <nikitaodessafreelance@gmail.com>
2021-08-21 20:39:42 +0300

    added adaptive for main page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
         <header className="header">
             <div className="container">
                 <div className="header__logo">
-                    <img width="38" src={logo} alt="Pizza logo" />
+                    <img src={logo} alt="Pizza logo" />
                     <div>
                     <h1>React Pizza</h1>
                     <p>самая вкусная пицца во вселенной</p>
@@ -18,8 +18,6 @@ const Header = () => {
                         <span>520 ₽</span>
                         <div className="button__delimiter"></div>
                         <svg
-                            width="18"
-                            height="18"
                             viewBox="0 0 18 18"
                             fill="none"
                             xmlns="http://www.w3.org/2000/svg">
